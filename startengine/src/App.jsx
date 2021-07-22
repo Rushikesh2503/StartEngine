b6e50d7dce51c52b6396b4aea5824b970382443c
Vaibhav Raj <vaibhavraj524@gmail.com>
2021-07-22 20:52:19 +0530

    routing successfully complted

--- startengine/src/App.jsx
+++ startengine/src/App.jsx
@@ -1,12 +1,15 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Components/pages/Home';
 import GetFunding from './Components/pages/GetFunding';
 
 function App() {
+
+
+
   return (
     <div className="app">
       <Router>
-      <Switch>
+        <Switch>
         <Route exact path="/"><Home/></Route>
         <Route path="/GetFunding"><GetFunding/></Route>
       </Switch>
--- startengine/src/Components/LandingPage/Achievements/Achievements.jsx
+++ startengine/src/Components/LandingPage/Achievements/Achievements.jsx
@@ -1,49 +1,63 @@
-import React from 'react';
-import styled from "./Achievements.module.css"
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styled from "./Achievements.module.css";
 
 const Achievements = () => {
-    return (
-        <div>
-            <div className={styled.frMaindivC}>
-                <div className={styled.frImg}>
-                    <img src="https://mk0blogstarteng8gb57.kinstacdn.com/wp-content/uploads/2021/06/500kusers_tw_white-1-1024x626.png" alt="500K USERS" />
-                </div>
-                <div className={styled.fr500kdescDiv}>
-                    <div className={styled.frTitle1}>
-                        <h1>Over <span className={styled.users500k}>500K Users</span> on StartEngine!</h1>
-                    </div>
-                    <div className={styled.frSubtitle2}> 
-                        <p>Join more than 500,000 people on StartEngine and build your investment portfolio. Welcome to the future of startup investing.</p>
-                    </div>
-                    <div className={styled.frBtns1}>
-                        <button className={styled.stInvesBtn1}>
-                        Start Investing
-                        </button>
-                        <button className={styled.getFundBtn1}>
-                        Get Funding
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className={styled.frMaindivC2}>
-                <div>
-                    <h1>500K</h1>
-                    <h2>Investors</h2>
-                    <p>More than 500,000 prospective investors are part of our community.</p>
-                </div>
-                <div>
-                    <h1>$350M</h1>
-                    <h2>Raised</h2>
-                    <p>Over $350M has been invested on StartEngine to date.</p>
-                </div>
-                <div>
-                    <h1>500+</h1>
-                    <h2>Offerings Funded</h2>
-                    <p>More than 500 offerings have been successfully funded on StartEngine.</p>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={styled.frMaindivC}>
+        <div className={styled.frImg}>
+          <img
+            src="https://mk0blogstarteng8gb57.kinstacdn.com/wp-content/uploads/2021/06/500kusers_tw_white-1-1024x626.png"
+            alt="500K USERS"
+          />
         </div>
-    )
-}
+        <div className={styled.fr500kdescDiv}>
+          <div className={styled.frTitle1}>
+            <h1>
+              Over <span className={styled.users500k}>500K Users</span> on
+              StartEngine!
+            </h1>
+          </div>
+          <div className={styled.frSubtitle2}>
+            <p>
+              Join more than 500,000 people on StartEngine and build your
+              investment portfolio. Welcome to the future of startup investing.
+            </p>
+          </div>
+          <div className={styled.frBtns1}>
+            <button className={styled.stInvesBtn1}>Start Investing</button>
+            <Link to="/GetFunding">
+              <button className={styled.getFundBtn1}>Get Funding</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styled.frMaindivC2}>
+        <div>
+          <h1>500K</h1>
+          <h2>Investors</h2>
+          <p>
+            More than 500,000 prospective investors are part of our community.
+          </p>
+        </div>
+        <div>
+          <h1>$350M</h1>
+          <h2>Raised</h2>
+          <p>Over $350M has been invested on StartEngine to date.</p>
+        </div>
+        <div>
+          <h1>500+</h1>
+          <h2>Offerings Funded</h2>
+          <p>
+            More than 500 offerings have been successfully funded on
+            StartEngine.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Achievements
+export default Achievements;
