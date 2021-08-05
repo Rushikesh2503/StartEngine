70bab43854552c7d2156330728db4fbb31cf4e61
Rushikesh2503 <rushikeshbambadkar7@gmail.com>
2021-08-05 17:12:51 +0530

    resp home and removed inline css

--- startengine/src/Components/LandingPage/Card/Card.jsx
+++ startengine/src/Components/LandingPage/Card/Card.jsx
@@ -1,34 +1,50 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
-import ShowCard from "./ShowCard";
-import styles from "./Card.module.css";
 import { v4 as uuid } from "uuid";
+import styles from "./Card.module.css";
+import styled from "../StartInvestment/CardsData.module.css";
+import ShowCard from "./ShowCard";
+import axios from "axios";
 
+export default function Card() {
+  const [data, setData] = React.useState([]);
 
-export default function card({ data }) {
+  const fetchData = async () => {
+    const response = await axios.get(`http://localhost:3001/data`);
+    const { data } = response;
+    setData(data);
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
   const handleShowData = (e) => {
     let check = e.title.split(" ")[0];
     if (check === "Fisher") {
-      window.location="http://localhost:3000/fisher"
-    }else if (check === "Piestro") {
-      window.location="http://localhost:3000/piestro"
-    }else if (check === "xCraft") {
-      window.location="http://localhost:3000/xCraft"
+      window.location = "http://localhost:3000/fisher";
+    } else if (check === "Piestro") {
+      window.location = "http://localhost:3000/piestro";
+    } else if (check === "xCraft") {
+      window.location = "http://localhost:3000/xCraft";
     }
-  }
-    const dataItem = data.map((item, i) => <div style={{ width: "100%", display: "flex", flexDirection: "column"}} key={i} >
-        <h2 styles={{ width: "100%"}} >{item.category}</h2>
-        <div className={styles.category} >
-        <div style={{ width: "100%", display: "flex", flexDirection: "row" }} key={uuid()} className={styles.cardsDivv}  >
-          {item.array.map(el =>
-            <div onClick={()=>handleShowData(el)} key={uuid()}><ShowCard el={el} /></div>)}
-        </div>
-        </div>
-  </div>);
-
-  return (
-    <div className={styles.main} >
-      {dataItem}
+  };
+  const dataItem = data.map((item, i) => (
+    <div className={styled.cardsMainDd} key={i}>
+      <h1 className={styles.headdingCate}>{item.category}</h1>
+      <div key={uuid()} className={styled.cardsDivv}>
+        {item.array.map((el) => (
+          <div
+            onClick={() => handleShowData(el)}
+            key={uuid()}
+            className={styled.cardsDivInnerd}
+          >
+            <ShowCard el={el} />
+          </div>
+        ))}
+      </div>
     </div>
-  );
-}
+  ));
 
+  return <div className={styles.main}>{dataItem}</div>;
+}
